04e99137f02be1f01f83f365645ae5835f7dd413
Leyland Yang <yangleyland@gmail.com>
2022-11-11 19:14:42 -0800

    gradient and login buttons

--- components/info/info.tsx
+++ components/info/info.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 export interface InfoProps {}
 
 const StyledInfo = styled.div`
-  background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(0,86,214,1) 100%);
+  background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(89,139,213,1) 100%);
   
   width: 100%;
   height: 100vh;
@@ -21,6 +21,8 @@ export function Info(props: InfoProps) {
         <HeaderText><Image src="/logo.png" alt={''}/></HeaderText>
         <SubText>live together. it's easy.</SubText>
         <Description>easily track chores and schedules with your roommates in real-time.</Description>
+        <LogInButton>Log-in</LogInButton>
+        <SignUpButton>Create Account</SignUpButton>
     </StyledInfo>
 ); 
 }
@@ -53,4 +55,56 @@ const Description = styled.div`
 `
 const Image = styled.img `
   width: 400px;
+`
+const LogInButton = styled.button`
+  width: 150px;
+  padding: 0;
+  border: none;
+  background: none;
+  cursor: pointer;
+  outline: 0;
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 16px;
+  border-radius: .5rem;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  :hover {
+      color: #fff;
+      background-color: #0b5ed7;
+      border-color: #0a58ca;
+  }           
+  margin-top: 20px;
+`
+const SignUpButton = styled.button`
+  width: 150px;
+  padding: 0;
+  border: none;
+  background: none;
+  cursor: pointer;
+  outline: 0;
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 16px;
+  border-radius: .5rem;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  :hover {
+      color: #fff;
+      background-color: #0b5ed7;
+      border-color: #0a58ca;
+  }           
+  margin: 10px;
 `
